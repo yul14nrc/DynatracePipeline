1da9d8c50edf4bde61d3d5e820ed8baacfdc526c
yul14nrc <61518345+yul14nrc@users.noreply.github.com>
2020-03-05 15:48:16 -0500

    update wget properties

--- L&P_test.sh
+++ L&P_test.sh
@@ -1,5 +1,7 @@
 #Downloading scommand to run the loadtest from cloudtest
 cloudtest_url=https://ec2-18-235-65-145.compute-1.amazonaws.com/concerto
 sc_zip_url=$cloudtest_url/downloads/scommand/scommand.zip
-wget --no-check-certificate -O scommand.zip $sc_zip_url
+wget --no-check-certificate --no-verbose -O scommand.zip $sc_zip_url
 unzip ./scommand.zip
+
+
